565b262b8d225d0e3e79952cd2c364dfcee5a493
srsaswin <s.saswin2005@gmail.com>
2025-06-28 09:27:22 +0530

    add rest

--- short-url/src/main/java/com/javaproject/shorturl/HomeController.java
+++ short-url/src/main/java/com/javaproject/shorturl/HomeController.java
@@ -1,15 +1,17 @@
 package com.javaproject.shorturl;
-
-import org.springframework.stereotype.Controller;
+ 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
-@Controller
+@RestController
 public class HomeController {
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("title", "hi from thymelea f");
         return "index";
     }
-
+ 
     @GetMapping("/r")
     public String demo(){
         return "demoRout";
